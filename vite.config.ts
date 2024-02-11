39951a7a0d428b63039c442081668cdfd77eb002
Mikhail <msl2002@yandex.ru>
2024-02-11 18:27:41 +0300

    fix for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dictionary-app',
+  base: '/dictionary-app-for-mc',
   plugins: [react()],
   resolve: {
     alias: {
